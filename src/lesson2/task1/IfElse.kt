7afcb2865677dbefbeb4bd73f16192ec5614d4f7
Julianne2212 <a_surkov_2013@mail.ru>
2022-09-15 03:04:14 +0600

    Решен первый lesson и есть вопросы по второму, не могу найти ошибку в друх задачах

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,7 +68,22 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    val x1 = "год"
+    val x2 = "года"
+    val x3 = "лет"
+    return when {
+        age == 0 || age == 9 || age == 10 || age == 11 || age == 12 || age == 13 || age == 14
+                || age == 15 || age == 16 || age == 17 || age == 18 || age == 19 || age == 20
+                || age == 111 -> "$age $x3"
+
+        age % 10 == 1 || age ==1 -> "$age $x1"
+        age % 10 == 2 || age % 10 == 3 || age % 10 == 4 -> "$age $x2"
+        age == 2 || age == 3 || age == 4 || age == 5 || age == 6 || age == 7 || age == 8 -> "$age$x2"
+
+        else -> "$age $x3"
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -81,7 +96,15 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    val s1 = t1*v1
+    val s2 = t2*v2
+    val s3 = t3*v3
+    val s = (s1+s2+s3)/2.0
+    return if (s1>=s) (s1/v1)
+    else if ((s>s1) && ((s1+s2)>=s)) ((s-s1/v2)+t1)
+    else  ((s-s1-s2/v3)+t1+t2)
+}
 
 /**
  * Простая (2 балла)
@@ -96,8 +119,14 @@ fun whichRookThreatens(
     kingX: Int, kingY: Int,
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
-): Int = TODO()
-
+): Any { return if ((kingX==rookX1) && (kingY==rookY1) || (kingX==rookX1) && (kingY!=rookY1) ||
+        (kingX!=rookX1) && (kingY==rookY1)) 1
+else if ((kingX==rookX2) && (kingY==rookY2) || (kingX==rookX2) && (kingY!=rookY2) ||
+(kingX!=rookX2) && (kingY==rookY2)) 2
+else if ((kingX!=rookX1) && (kingY!=rookY1) && (kingX!=rookX2) && (kingY!=rookY2)) 0
+else if (((kingX==rookX1) || (kingY==rookY1)) && ((kingX==rookX2) || (kingY==rookY2))) 3
+    else println("ответа нет")
+}
 /**
  * Простая (2 балла)
  *
